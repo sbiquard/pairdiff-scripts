4ecaf8a22d8961cb04454c83fbebe2587b43d663
Simon Biquard <sbiquard@gmail.com>
2024-03-04 15:35:57 -0800

    option to change file name

--- get_mask_apo.py
+++ get_mask_apo.py
@@ -22,6 +22,12 @@ def add_arguments(parser):
         type=float,
         help="scale of apodization in degrees (default: 10 deg)",
     )
+    parser.add_argument(
+        "--out",
+        dest="file_name",
+        default="mask_apo",
+        help="file name under which to save the mask (must not already exist)",
+    )
 
 
 def main(args):
@@ -35,14 +41,16 @@ def main(args):
     mask = spectrum.get_mask_apo(hits, args.min_hits, args.aposize)
 
     # Save the mask
-    fname_save = "out/mask_apo.fits"
+    fname_save = f"out/{args.file_name}.fits"
     print(f"Save apodized mask under '{fname_save}'")
-    hp.write_map(fname_save, mask, overwrite=True)
+    hp.write_map(fname_save, mask, overwrite=False)
 
     # Plot the mask
-    fname_plot = "out/mask_apo.png"
+    fname_plot = f"out/{args.file_name}.png"
     print(f"Save mask plot under '{fname_plot}'")
-    hp.projview(mask, title="Apodized mask")
+    hp.projview(
+        mask, title=f"Apodized mask (minhits={args.min_hits}, aposize={args.aposize})"
+    )
     plt.savefig(fname_plot)
 
 
