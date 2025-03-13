76acaba394723bf4d6f0d6f19df85b7cc0acc119
Simon Biquard <sbiquard+git@gmail.com>
2025-03-13 15:16:27 +0100

    Add mask_apo fits files to rsync include pattern

--- rsync_jz.sh
+++ rsync_jz.sh
@@ -3,6 +3,7 @@
 # Pull plots and spectra from Jean-Zay
 rsync -ravuLPh \
     --include 'opti/atm/ml/Hits_*.fits' \
+    --include 'mask_apo*.fits' \
     --exclude '*.fits' \
     jean-zay:/lustre/fswork/projects/rech/nih/usl22vm/repos/pairdiff-scripts/out/ \
     jz_out
