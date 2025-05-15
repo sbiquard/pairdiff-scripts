6ddd0e8ce1c65701d71d1133d185433fdfe452dc
Simon Biquard <sbiquard+git@gmail.com>
2025-05-15 18:05:09 +0200

    debugging

--- tests/leak/run.noiseless.sh
+++ tests/leak/run.noiseless.sh
@@ -4,11 +4,11 @@ export OMP_NUM_THREADS=8
 ntask=2
 
 schedule="schedules/schedule.opti.txt"
-cmb_input="ffp10_lensed_scl_100_nside0512.fits"
-# cmb_input="qu_nside0512.fits"
-# cmb_input="0064.fits"
+# sky="ffp10_lensed_scl_100_nside0512.fits"
+# sky="qu_nside0512.fits"
+sky="qu_nside0064.fits"
 
-outdir=out/leak/noiseless
+outdir=out/leak/noiseless_qu
 mkdir -p $outdir/ml
 mkdir -p $outdir/pd
 
@@ -17,35 +17,39 @@ logfile=$outdir/ml/run.log
 echo "Writing $logfile"
 mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
-    --variable_model.scatter 0 \
+    $(< no_scatter.par) \
+    --full_pointing \
+    --save_hdf5.enable \
+    --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
+    --save_hdf5.shared "['flags', 'times']" \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --pixels_healpix_radec.nside 64 \
+    --mappraiser.nside 64 \
     --mappraiser.binned \
     --mappraiser.zero_noise \
     --thinfp 64 \
-    --schedule $schedule \
-    --scan_map.file $cmb_input \
     --out $outdir/ml \
     >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
 
-# PD run
-logfile=$outdir/pd/run.log
-echo "Writing $logfile"
-mpirun -np $ntask ./so_mappraiser.py \
-    $(< sat.par) \
-    --variable_model.scatter 0 \
-    --mappraiser.binned \
-    --mappraiser.zero_noise \
-    --mappraiser.pair_diff \
-    --thinfp 64 \
-    --schedule $schedule \
-    --scan_map.file $cmb_input \
-    --out $outdir/pd \
-    >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
+# # PD run
+# logfile=$outdir/pd/run.log
+# echo "Writing $logfile"
+# mpirun -np $ntask ./so_mappraiser.py \
+#     $(< sat.par) \
+#     $(< no_scatter.par) \
+#     --schedule $schedule \
+#     --scan_map.file $sky \
+#     --pixels_healpix_radec.nside 64 \
+#     --mappraiser.nside 64 \
+#     --mappraiser.binned \
+#     --mappraiser.zero_noise \
+#     --mappraiser.pair_diff \
+#     --thinfp 64 \
+#     --out $outdir/pd \
+#     >$logfile 2>&1
 
-outdir=out/leak/noiseless_nohwp
+outdir=out/leak/noiseless_qu_noflags
 mkdir -p $outdir/ml
 mkdir -p $outdir/pd
 
@@ -54,34 +58,41 @@ logfile=$outdir/ml/run.log
 echo "Writing $logfile"
 mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
-    $(< no_hwp.par) \
-    --variable_model.scatter 0 \
+    $(< no_flags.par) \
+    $(< no_scatter.par) \
+    --full_pointing \
+    --save_hdf5.enable \
+    --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
+    --save_hdf5.shared "['flags', 'times']" \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --pixels_healpix_radec.nside 64 \
+    --mappraiser.nside 64 \
     --mappraiser.binned \
     --mappraiser.zero_noise \
     --thinfp 64 \
-    --schedule $schedule \
-    --scan_map.file $cmb_input \
     --out $outdir/ml \
     >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
 
-# PD run
-logfile=$outdir/pd/run.log
-echo "Writing $logfile"
-mpirun -np $ntask ./so_mappraiser.py \
-    $(< sat.par) \
-    $(< no_hwp.par) \
-    --variable_model.scatter 0 \
-    --mappraiser.binned \
-    --mappraiser.zero_noise \
-    --mappraiser.pair_diff \
-    --thinfp 64 \
-    --schedule $schedule \
-    --scan_map.file $cmb_input \
-    --out $outdir/pd \
-    >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
+# # PD run
+# logfile=$outdir/pd/run.log
+# echo "Writing $logfile"
+# mpirun -np $ntask ./so_mappraiser.py \
+#     $(< sat.par) \
+#     $(< no_flags.par) \
+#     $(< no_scatter.par) \
+#     --schedule $schedule \
+#     --scan_map.file $sky \
+#     --pixels_healpix_radec.nside 64 \
+#     --mappraiser.nside 64 \
+#     --mappraiser.binned \
+#     --mappraiser.zero_noise \
+#     --mappraiser.pair_diff \
+#     --thinfp 64 \
+#     --out $outdir/pd \
+#     >$logfile 2>&1
 
 echo "$(date) : Done!"
+echo "Plotting..."
+
+python post/plot_maps_all.py -r out/leak --sky $sky
