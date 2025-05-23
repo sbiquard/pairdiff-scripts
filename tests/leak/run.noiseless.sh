2c48916ef8b8b66ce84288749944ef7ba462890a
Simon Biquard <sbiquard+git@gmail.com>
2025-05-23 15:25:53 +0200

    noiseless case fixed

--- tests/leak/run.noiseless.sh
+++ tests/leak/run.noiseless.sh
@@ -4,11 +4,11 @@ export OMP_NUM_THREADS=8
 ntask=2
 
 schedule="schedules/schedule.opti.txt"
-# sky="ffp10_lensed_scl_100_nside0512.fits"
+sky="ffp10_lensed_scl_100_nside0512.fits"
 # sky="qu_nside0512.fits"
-sky="qu_nside0064.fits"
+# sky="0064.fits"
 
-outdir=out/leak/noiseless_qu
+outdir=out/leak/noiseless
 mkdir -p $outdir/ml
 mkdir -p $outdir/pd
 
@@ -18,38 +18,38 @@ echo "Writing $logfile"
 mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
     $(< no_scatter.par) \
-    --full_pointing \
-    --save_hdf5.enable \
-    --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
-    --save_hdf5.shared "['flags', 'times']" \
     --schedule $schedule \
     --scan_map.file $sky \
-    --pixels_healpix_radec.nside 64 \
-    --mappraiser.nside 64 \
     --mappraiser.binned \
     --mappraiser.zero_noise \
     --thinfp 64 \
     --out $outdir/ml \
     >$logfile 2>&1
+    # --pixels_healpix_radec.nside 64 \
+    # --mappraiser.nside 64 \
+    # --full_pointing \
+    # --save_hdf5.enable \
+    # --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
+    # --save_hdf5.shared "['flags', 'times']" \
 
-# # PD run
-# logfile=$outdir/pd/run.log
-# echo "Writing $logfile"
-# mpirun -np $ntask ./so_mappraiser.py \
-#     $(< sat.par) \
-#     $(< no_scatter.par) \
-#     --schedule $schedule \
-#     --scan_map.file $sky \
-#     --pixels_healpix_radec.nside 64 \
-#     --mappraiser.nside 64 \
-#     --mappraiser.binned \
-#     --mappraiser.zero_noise \
-#     --mappraiser.pair_diff \
-#     --thinfp 64 \
-#     --out $outdir/pd \
-#     >$logfile 2>&1
+# PD run
+logfile=$outdir/pd/run.log
+echo "Writing $logfile"
+mpirun -np $ntask ./so_mappraiser.py \
+    $(< sat.par) \
+    $(< no_scatter.par) \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --mappraiser.binned \
+    --mappraiser.zero_noise \
+    --mappraiser.pair_diff \
+    --thinfp 64 \
+    --out $outdir/pd \
+    >$logfile 2>&1
+    # --pixels_healpix_radec.nside 64 \
+    # --mappraiser.nside 64 \
 
-outdir=out/leak/noiseless_qu_noflags
+outdir=out/leak/noiseless_noflags
 mkdir -p $outdir/ml
 mkdir -p $outdir/pd
 
@@ -60,37 +60,37 @@ mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
     $(< no_flags.par) \
     $(< no_scatter.par) \
-    --full_pointing \
-    --save_hdf5.enable \
-    --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
-    --save_hdf5.shared "['flags', 'times']" \
     --schedule $schedule \
     --scan_map.file $sky \
-    --pixels_healpix_radec.nside 64 \
-    --mappraiser.nside 64 \
     --mappraiser.binned \
     --mappraiser.zero_noise \
     --thinfp 64 \
     --out $outdir/ml \
     >$logfile 2>&1
+    # --pixels_healpix_radec.nside 64 \
+    # --mappraiser.nside 64 \
+    # --full_pointing \
+    # --save_hdf5.enable \
+    # --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
+    # --save_hdf5.shared "['flags', 'times']" \
 
-# # PD run
-# logfile=$outdir/pd/run.log
-# echo "Writing $logfile"
-# mpirun -np $ntask ./so_mappraiser.py \
-#     $(< sat.par) \
-#     $(< no_flags.par) \
-#     $(< no_scatter.par) \
-#     --schedule $schedule \
-#     --scan_map.file $sky \
-#     --pixels_healpix_radec.nside 64 \
-#     --mappraiser.nside 64 \
-#     --mappraiser.binned \
-#     --mappraiser.zero_noise \
-#     --mappraiser.pair_diff \
-#     --thinfp 64 \
-#     --out $outdir/pd \
-#     >$logfile 2>&1
+# PD run
+logfile=$outdir/pd/run.log
+echo "Writing $logfile"
+mpirun -np $ntask ./so_mappraiser.py \
+    $(< sat.par) \
+    $(< no_flags.par) \
+    $(< no_scatter.par) \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --mappraiser.binned \
+    --mappraiser.zero_noise \
+    --mappraiser.pair_diff \
+    --thinfp 64 \
+    --out $outdir/pd \
+    >$logfile 2>&1
+    # --pixels_healpix_radec.nside 64 \
+    # --mappraiser.nside 64 \
 
 echo "$(date) : Done!"
 echo "Plotting..."
