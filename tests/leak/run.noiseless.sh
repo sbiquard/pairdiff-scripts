37950740c78fc356cc3296aee39145531e74dd97
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2025-06-06 10:59:38 +0200

    noiseless test script

--- tests/leak/run.noiseless.sh
+++ tests/leak/run.noiseless.sh
@@ -5,56 +5,13 @@ ntask=2
 
 schedule="schedules/schedule.opti.txt"
 sky="ffp10_lensed_scl_100_nside0512.fits"
-# sky="qu_nside0512.fits"
-# sky="0064.fits"
+# sky="nside0128.fits"
+nside=512
 
-outdir=out/leak/noiseless
-mkdir -p $outdir/ml
-mkdir -p $outdir/pd
+outdir=out/leak/noiseless/incl
+mkdir -p $outdir
 
-# ML run
-logfile=$outdir/ml/run.log
-echo "Writing $logfile"
-mpirun -np $ntask ./so_mappraiser.py \
-    $(< sat.par) \
-    $(< no_scatter.par) \
-    --schedule $schedule \
-    --scan_map.file $sky \
-    --mappraiser.binned \
-    --mappraiser.zero_noise \
-    --thinfp 64 \
-    --out $outdir/ml \
-    >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
-    # --full_pointing \
-    # --save_hdf5.enable \
-    # --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
-    # --save_hdf5.shared "['flags', 'times']" \
-
-# PD run
-logfile=$outdir/pd/run.log
-echo "Writing $logfile"
-mpirun -np $ntask ./so_mappraiser.py \
-    $(< sat.par) \
-    $(< no_scatter.par) \
-    --schedule $schedule \
-    --scan_map.file $sky \
-    --mappraiser.binned \
-    --mappraiser.zero_noise \
-    --mappraiser.pair_diff \
-    --thinfp 64 \
-    --out $outdir/pd \
-    >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
-
-outdir=out/leak/noiseless_noflags
-mkdir -p $outdir/ml
-mkdir -p $outdir/pd
-
-# ML run
-logfile=$outdir/ml/run.log
+logfile=$outdir/run.log
 echo "Writing $logfile"
 mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
@@ -62,37 +19,72 @@ mpirun -np $ntask ./so_mappraiser.py \
     $(< no_scatter.par) \
     --schedule $schedule \
     --scan_map.file $sky \
+    --pixels_healpix_radec.nside $nside \
+    --mappraiser.nside $nside \
     --mappraiser.binned \
     --mappraiser.zero_noise \
     --thinfp 64 \
-    --out $outdir/ml \
+    --out $outdir \
     >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
-    # --full_pointing \
-    # --save_hdf5.enable \
-    # --save_hdf5.detdata "['signal', 'flags', 'pixels', 'weights']" \
-    # --save_hdf5.shared "['flags', 'times']" \
 
-# PD run
-logfile=$outdir/pd/run.log
+outdir=out/leak/noiseless/excl
+mkdir -p $outdir
+
+logfile=$outdir/run.log
+echo "Writing $logfile"
+mpirun -np $ntask ./so_mappraiser.py \
+    $(< sat.par) \
+    $(< no_scatter.par) \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --pixels_healpix_radec.nside $nside \
+    --mappraiser.nside $nside \
+    --mappraiser.binned \
+    --mappraiser.zero_noise \
+    --thinfp 64 \
+    --out $outdir \
+    >$logfile 2>&1
+
+outdir=out/leak/noiseless/incl_nohwp
+mkdir -p $outdir
+
+logfile=$outdir/run.log
 echo "Writing $logfile"
 mpirun -np $ntask ./so_mappraiser.py \
     $(< sat.par) \
     $(< no_flags.par) \
     $(< no_scatter.par) \
+    $(< no_hwp.par) \
     --schedule $schedule \
     --scan_map.file $sky \
+    --pixels_healpix_radec.nside $nside \
+    --mappraiser.nside $nside \
     --mappraiser.binned \
     --mappraiser.zero_noise \
-    --mappraiser.pair_diff \
     --thinfp 64 \
-    --out $outdir/pd \
+    --out $outdir \
     >$logfile 2>&1
-    # --pixels_healpix_radec.nside 64 \
-    # --mappraiser.nside 64 \
+
+outdir=out/leak/noiseless/excl_nohwp
+mkdir -p $outdir
+
+logfile=$outdir/run.log
+echo "Writing $logfile"
+mpirun -np $ntask ./so_mappraiser.py \
+    $(< sat.par) \
+    $(< no_scatter.par) \
+    $(< no_hwp.par) \
+    --schedule $schedule \
+    --scan_map.file $sky \
+    --pixels_healpix_radec.nside $nside \
+    --mappraiser.nside $nside \
+    --mappraiser.binned \
+    --mappraiser.zero_noise \
+    --thinfp 64 \
+    --out $outdir \
+    >$logfile 2>&1
 
 echo "$(date) : Done!"
 echo "Plotting..."
 
-python post/plot_maps_all.py -v -r out/leak --sky $sky
+python post/plot_maps_all.py -v -r out/leak/noiseless --sky $sky --hits-percentile 0
