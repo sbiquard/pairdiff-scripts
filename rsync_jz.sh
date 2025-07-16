c584e28237ff4f82b019040a783b970c06fce9ab
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2025-07-16 15:49:33 +0200

    dont rsync old files

--- rsync_jz.sh
+++ rsync_jz.sh
@@ -6,5 +6,6 @@ rsync -ravuLPh \
     --include 'mask_apo*.fits' \
     --include 'leak/**/*.fits' \
     --exclude '*.fits' \
+    --exclude 'leak/old_incl_turnarounds' \
     jean-zay:/lustre/fswork/projects/rech/nih/usl22vm/repos/pairdiff-scripts/out/ \
     jz_out
