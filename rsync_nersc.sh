0a376c2f5b5279610ec92482f3c807f609c5bbd1
Simon Biquard <sbiquard+git@gmail.com>
2025-09-01 13:41:56 +0200

    fix sync script

--- rsync_nersc.sh
+++ rsync_nersc.sh
@@ -5,5 +5,5 @@ rsync -ravuLPh \
     --exclude '*.fits' \
     --exclude 'var_noise_model_data' \
     --exclude 'noise_psd_fit' \
-    perlmutter:/global/homes/s/sbiquard/pairdiff-scripts/out \
+    perlmutter:/global/homes/s/sbiquard/pairdiff-scripts/out/ \
     nersc_out
